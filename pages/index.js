669bb2375e9e16e4a910bb30cb1022cb69c4b3e2
JulsD <dutchak.julia@gmail.com>
2021-09-19 21:19:49 +0200

    add tailwind css

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div className="tw-p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">Welcome to Next.js!</div>
   }
   
   export default HomePage
